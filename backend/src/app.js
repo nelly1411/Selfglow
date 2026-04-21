4c25eaf978b6b990322ed5be7ca4c7970f41c19b
yichenzhong <yichenpsy@gmail.com>
2026-04-21 14:45:30 +0200

    add some comments for backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,19 @@
-const express = require("express");
+const express = require("express"); // import packages
 const cors = require("cors");
 const dotenv = require("dotenv");
 
-dotenv.config();
+dotenv.config(); // load the variables from .env
 
-const app = express();
+const app = express(); // create the main backend application
 
-// Middleware
-app.use(cors());
-app.use(express.json());
+// Middleware: processing steps
+app.use(cors());  // allow cross-origin access
+app.use(express.json()); // raw json -> JavaScript object, req.body works
 
 // Test route
 app.get("/", (req, res) => {
   res.send("SelfGlow backend is running");
-});
+}); 
 
 app.get("/api/test", (req, res) => {
   res.json({ message: "API works" });
